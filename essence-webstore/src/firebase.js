d9478ae429d8b837e1144a04adaa091ec7427766
DeepanshuPratik <deepanshu.pratik@gmail.com>
2022-03-07 15:32:48 +0530

    Nav Bar Basics

---
+++ essence-webstore/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDutAHPbQsm6aeHb7wAjZWIK4xk_LFHl48",
+    authDomain: "essence-webstore.firebaseapp.com",
+    projectId: "essence-webstore",
+    storageBucket: "essence-webstore.appspot.com",
+    messagingSenderId: "652091095653",
+    appId: "1:652091095653:web:d5fe91e166d138f70c248c",
+    measurementId: "G-4KGP41WRR1"
+  };
