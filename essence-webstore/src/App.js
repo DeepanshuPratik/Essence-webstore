65d2d2d08bb0841f121e9433e2dd55db33032c0c
DeepanshuPratik <deepanshu.pratik@gmail.com>
2022-03-08 11:00:59 +0530

    Home page done and checkout page start

--- essence-webstore/src/App.js
+++ essence-webstore/src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Checkout from './Checkout';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+        <Route path = "/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path = "/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
